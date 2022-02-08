f45cff145afd2cb0b2962be7d88fcead7877218a
Robinsoncifuentes <robinsonmunoz4@gmail.com>
2022-02-08 17:12:00 +0000

    subido al repo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#la-excusa").innerHTML = generarExcusa();
+  });
+};
+let generarExcusa = () => {
+  let sujeto = ["El perro ", "Mi abuela ", "La tortuga ", "Mi pajaro "];
+  let action = ["se comio ", " orinó ", "aplastó ", "rompió "];
+  let what = ["mi tarea ", "las llaves ", "el auto ", "la casa "];
+  let when = [
+    "antes de clases",
+    "justo ahora",
+    "al acabar ",
+    "durante el almuerzo",
+    "mientras rezaba"
+  ];
+  let sujetoIndex = Math.floor(Math.random() * sujeto.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  return (
+    sujeto[sujetoIndex] +
+    "" +
+    action[actionIndex] +
+    "" +
+    what[whatIndex] +
+    "" +
+    when[whenIndex]
+  );
 };
